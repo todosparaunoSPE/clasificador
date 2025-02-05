c26d6ad8ebb635f3de3f121677a13e08a0d11e4e
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-02-04 18:21:56 -0600

    Update appprincipal.py

--- appprincipal.py
+++ appprincipal.py
@@ -9,6 +9,24 @@ import streamlit as st
 import nltk
 from nltk.corpus import stopwords
 
+
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
 # Descargar stopwords si no están disponibles
 nltk.download('stopwords')
 stop_words = stopwords.words('spanish')
